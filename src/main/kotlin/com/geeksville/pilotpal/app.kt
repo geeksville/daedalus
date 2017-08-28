de9e9afb6460cf40cdf4ab26ea2ebaea616e8cd7
Kevin Hester <kevinh@geeksville.com>
2017-08-28 10:15:15 -0700

    basic position reading works

--- src/main/kotlin/com/geeksville/pilotpal/app.kt
+++ src/main/kotlin/com/geeksville/pilotpal/app.kt
@@ -1,5 +1,27 @@
 package com.geeksville.pilotpal
 
+import de.taimos.gpsd4java.api.ObjectListener
+import de.taimos.gpsd4java.backend.GPSdEndpoint
+import de.taimos.gpsd4java.backend.ResultParser
+import de.taimos.gpsd4java.types.TPVObject
+
 fun main(args: Array<String>) {
     println("hello world!")
+
+    val ep = GPSdEndpoint("localhost", 2947, ResultParser())
+
+    class MyListener : ObjectListener() {
+
+        override fun handleTPV(tpv: TPVObject) {
+            println("tpv: $tpv")
+        }
+        
+    }
+
+    ep.addListener(MyListener())
+    ep.start()
+    println("GPSD version: " + ep.version())
+    println("GPSD watch: " + ep.watch(true, true))
+    println("start pollining" + ep.poll())
+    Thread.sleep(60 * 1000)
 }
